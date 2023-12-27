808823efec52e5eaba011e9172785d5c080b6045
vsheva <valera.sheva@gmail.com>
2023-12-27 00:01:59 -0500

    add validation

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 import {Ingredient} from "../../shared/ingredient.model";
 import {ShoppingListService} from "../shoppig-list.service";
 import {NgForm} from "@angular/forms";
@@ -13,6 +13,7 @@ export class ShoppingEditComponent {
   }
 
   onAddIngredient(form: NgForm) {
+    //console.log("form", form) //{name: "..", amount: ".."}
     const value= form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     this.shoppingListService.addIngredient(newIngredient)
