0bc8e8c72c20228a0d4cc1819f085e2465eda5b8
vsheva <valera.sheva@gmail.com>
2023-12-28 13:16:42 -0500

    populate edit form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
+import {FormControl, FormGroup} from "@angular/forms";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -8,16 +10,43 @@ import {ActivatedRoute, Params} from "@angular/router";
 })
 export class RecipeEditComponent {
   id: number;
-  editMode:boolean = false;
+  editMode: boolean = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) {
   }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
       this.editMode = params.id != null; //!!
-      console.log(this.editMode);
+      this.initForm(); //call function
+      console.log("editMode",this.editMode);
+    })
+  }
+
+  onSubmit() {
+    console.log("recipeForm", this.recipeForm);
+  }
+
+  private initForm() {
+    let recipeName = "";
+    let recipeImagePath = "";
+    let recipeDescription = "";
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      console.log('this.recipeService.getRecipe(this.id)',this.recipeService.getRecipe(this.id))//{name:"Schnitzel", description: "", imagePath:"", ingredients:[1,2,3] }
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName), //[formControlName]="'name'"   'name' goes in html on line 17
+      'imagePath': new FormControl(recipeImagePath),
+      'description': new FormControl(recipeDescription), //[formControlName] = "'description'"
     })
   }
 
