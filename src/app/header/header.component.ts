08537a9c0221e04613254a3af4f460ca5fdae667
vsheva <valera.sheva@gmail.com>
2024-01-10 14:43:32 -0500

    store recipes on firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {DataStorageService} from "../shared/data-storage.service";
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,11 @@ import {Component, EventEmitter, Output} from '@angular/core';
 
 export class HeaderComponent {
   collapsed = true;
+
+  constructor(private dataStorageService: DataStorageService) {
+  }
+
+  onSaveData() {
+ this.dataStorageService.storeRecipes()
+  }
 }
