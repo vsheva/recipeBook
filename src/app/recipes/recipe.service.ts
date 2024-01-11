dd2b77993ca570c4db6c17e153ff758cc6c65973
vsheva <valera.sheva@gmail.com>
2024-01-10 23:47:00 -0500

    remove dummy data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,28 +12,30 @@ export class RecipeService {
   //recipeSelected = new EventEmitter<Recipe>()
   //recipeSelected = new Subject<Recipe>();???
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'Super tasty!',
-      'https://images.pexels.com/photos/1352270/pexels-photo-1352270.jpeg',
-      [
-        new Ingredient("Meat", 1),
-        new Ingredient("Cheese", 5),
-        new Ingredient("Tomatoes", 2),
-      ]
-    ),
-    new Recipe(
-      'Big Burger',
-      'Delicious!',
-      'https://images.unsplash.com/photo-1553979459-d2229ba7433b?q=80&w=3768&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      [
-        new Ingredient("Buns", 2),
-        new Ingredient("French Fries", 10),
-        new Ingredient("Souse", 1),
-      ]
-    )
-  ]
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'Super tasty!',
+  //     'https://images.pexels.com/photos/1352270/pexels-photo-1352270.jpeg',
+  //     [
+  //       new Ingredient("Meat", 1),
+  //       new Ingredient("Cheese", 5),
+  //       new Ingredient("Tomatoes", 2),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Big Burger',
+  //     'Delicious!',
+  //     'https://images.unsplash.com/photo-1553979459-d2229ba7433b?q=80&w=3768&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  //     [
+  //       new Ingredient("Buns", 2),
+  //       new Ingredient("French Fries", 10),
+  //       new Ingredient("Souse", 1),
+  //     ]
+  //   )
+  // ]
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {
   }
