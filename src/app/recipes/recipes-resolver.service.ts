0858a95897b634777088da2e36559584fc5ea606
vsheva <valera.sheva@gmail.com>
2024-01-11 14:11:34 -0500

    fix bug

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -2,14 +2,24 @@ import {Injectable} from "@angular/core";
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
 import {Recipe} from "./recipe.model";
 import {DataStorageService} from "../shared/data-storage.service";
+import {RecipeService} from "./recipe.service";
 
 @Injectable({providedIn: "root"}) //!!!if inject service here
 export class RecipesResolverService implements Resolve<Recipe[]> {
 
-  constructor(private dataStorageService: DataStorageService) {
+  constructor(private dataStorageService: DataStorageService,
+              private recipeService: RecipeService
+              ) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipeService.getRecipes();//!!! есть
+
+    if(recipes.length=== 0) {
+      return this.dataStorageService.fetchRecipes(); //!!!fetch
+    } else {
+      return recipes;
+    }
+
   }
 }
