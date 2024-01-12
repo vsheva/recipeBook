197d9f9275cf068e82907a9e823ed32ce1b8b572
vsheva <valera.sheva@gmail.com>
2024-01-11 22:06:56 -0500

    add auth component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,22 +6,24 @@ import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.componen
 import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
 import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
 import {RecipesResolverService} from "./recipes/recipes-resolver.service";
+import {AuthComponent} from "./auth/auth.component";
 
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
 
   {
     path: 'recipes', component: RecipesComponent,
     children: [
-      { path: '', component: RecipeStartComponent },
-      { path: 'new', component: RecipeEditComponent }, //не менять местами с :id
-      { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]}, //red!
-      {  path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] }, //red!
+      {path: '', component: RecipeStartComponent},
+      {path: 'new', component: RecipeEditComponent}, //не менять местами с :id
+      {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]}, //red!
+      {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}, //red!
 
     ]
   },
-  { path: 'shopping-list', component: ShoppingListComponent },
+  {path: 'shopping-list', component: ShoppingListComponent},
+  {path: 'auth', component: AuthComponent},
 
 
 ]
--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,11 +1,11 @@
 import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot} from "@angular/router";
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
 import {Recipe} from "./recipe.model";
 import {DataStorageService} from "../shared/data-storage.service";
 import {RecipeService} from "./recipe.service";
 
 @Injectable({providedIn: "root"}) //!!!if inject service here
-export class RecipesResolverService implements ResolveFn<Recipe[]> {
+export class RecipesResolverService implements Resolve<Recipe[]> {
 
   constructor(private dataStorageService: DataStorageService,
               private recipeService: RecipeService
