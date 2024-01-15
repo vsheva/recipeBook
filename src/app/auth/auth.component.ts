09e2275b6a9010cc5c25187efabf37b288e24326
vsheva <valera.sheva@gmail.com>
2024-01-14 23:57:45 -0500

    auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 import {NgForm} from '@angular/forms'
-import {AuthResponse, AuthService} from "./auth.service";
+import {AuthResponseData, AuthService} from "./auth.service";
 import {Observable} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-auth',
@@ -14,57 +15,96 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   onSwitchMode() {
     this.isLogin = !this.isLogin
   }
 
-  onSubmit(forma: NgForm) {
-    if (!forma.valid) {
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
       return;
     }
-    const email = forma.value.email;
-    const password = forma.value.password;
-    let authObservable: Observable<AuthResponse>;
+    const email = form.value.email;
+    const password = form.value.password;
+    let authObservable: Observable<AuthResponseData>;
 
     this.isLoading = true;
     if (this.isLogin) {
       authObservable = this.authService.login(email, password)
-      // .subscribe(resData => {
-      //   console.log(resData)
-      //   this.isLoading = false;
-      // },
-      // errorMessage => {//we subscribe to the observable  -that  moved logic to service in pipe (в пайп мы чуть подстраиваем данные)
-      //   console.log(errorMessage);
-      //   this.error = errorMessage;
-      //   this.isLoading = false;
-      // })
     } else {
       authObservable = this.authService.signup(email, password)
-      // .subscribe(resData => {
-      //     console.log(resData)
-      //     this.isLoading = false;
-      //   },
-      //   errorMessage => {//we subscribe to the observable  -that  moved logic to service in pipe (в пайп мы чуть подстраиваем данные)
-      //     console.log(errorMessage);
-      //     this.error = errorMessage;
-      //     this.isLoading = false;
-      //   })
     }
 
     authObservable.subscribe(resData => {
         console.log(resData)
         this.isLoading = false;
+        this.router.navigate(["/recipes"]);
       },
-      errorMessage => {//we subscribe to the observable  -that  moved logic to service in pipe (в пайп мы чуть подстраиваем данные)
+      errorMessage => {
         console.log(errorMessage);
         this.error = errorMessage;
         this.isLoading = false;
       })
-    forma.reset();
+    form.reset();
   }
 }
 
-//!!!we suscribe to observable (that is message) -> and throw it through throwError(errorMessage)
+
+/*
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+import { AuthService, AuthResponseData } from './auth.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html'
+})
+export class AuthComponent {
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
+    } else {
+      authObs = this.authService.signup(email, password);
+    }
+
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+
+    form.reset();
+  }
+}
+*/
+
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,40 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {DataStorageService} from "../shared/data-storage.service";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: './header.component.html'
 })
-
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
   collapsed = true;
 
-  constructor(private dataStorageService: DataStorageService) {
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = !!user;
+      console.log(!user);
+      console.log(!!user);
+    });
   }
 
   onSaveData() {
- this.dataStorageService.storeRecipes()
+    this.dataStorageService.storeRecipes();
   }
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
