ac1034cf06c166bb4e24ec251434505f5f4d29e0
vsheva <valera.sheva@gmail.com>
2024-01-29 22:58:34 -0500

    fix

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -32,11 +32,7 @@ export class AuthGuard implements CanActivate {
         }
         return this.router.createUrlTree(['/auth']);
       })
-      // tap(isAuth => {
-      //   if (!isAuth) {
-      //     this.router.navigate(['/auth']);
-      //   }
-      // })
+
     );
   }
 }
