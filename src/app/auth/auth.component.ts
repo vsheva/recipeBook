f916d8de94979ca64031af067e5a60c4bb1a8bd2
vsheva <valera.sheva@gmail.com>
2024-01-31 23:27:19 -0500

    1/31/2024

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, ComponentFactoryResolver} from '@angular/core';
 import {NgForm} from '@angular/forms'
 import {AuthResponseData, AuthService} from "./auth.service";
 import {Observable} from "rxjs";
 import {Router} from "@angular/router";
+import {AlertComponent} from "../shared/alert/alert.component";
 
 @Component({
   selector: 'app-auth',
@@ -15,8 +16,10 @@ export class AuthComponent {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService, private router: Router) {
-  }
+  constructor(private authService: AuthService,
+              private router: Router,
+              private componentFactoryResolver: ComponentFactoryResolver
+              ) {}
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode
@@ -45,70 +48,21 @@ export class AuthComponent {
       errorMessage => {
         console.log(errorMessage);
         this.error = errorMessage;
+        this.showErrorAlert(errorMessage);
         this.isLoading = false;
       })
     form.reset();
   }
-}
 
-
-/*
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-
-import { AuthService, AuthResponseData } from './auth.service';
-
-@Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html'
-})
-export class AuthComponent {
-  isLoginMode = true;
-  isLoading = false;
-  error: string = null;
-
-  constructor(private authService: AuthService, private router: Router) {}
-
-  onSwitchMode() {
-    this.isLoginMode = !this.isLoginMode;
+  onHandleError() {
+    this.error = null;
   }
 
-  onSubmit(form: NgForm) {
-    if (!form.valid) {
-      return;
-    }
-    const email = form.value.email;
-    const password = form.value.password;
-
-    let authObs: Observable<AuthResponseData>;
-
-    this.isLoading = true;
-
-    if (this.isLoginMode) {
-      authObs = this.authService.login(email, password);
-    } else {
-      authObs = this.authService.signup(email, password);
-    }
-
-    authObs.subscribe(
-      resData => {
-        console.log(resData);
-        this.isLoading = false;
-        this.router.navigate(['/recipes']);
-      },
-      errorMessage => {
-        console.log(errorMessage);
-        this.error = errorMessage;
-        this.isLoading = false;
-      }
-    );
-
-    form.reset();
+  private showErrorAlert(message: string) {
+  //const alertCmp = new AlertComponent()
+   const alertCmpFactory= this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
   }
 }
 
-*/
 
 
