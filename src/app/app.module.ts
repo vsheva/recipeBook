871aadfc15a776dc354e6e1f1f921f2dacb003e9
vsheva <valera.sheva@gmail.com>
2024-02-08 22:22:50 -0500

    add shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
-import {AlertComponent} from "./shared/alert/alert.component";
 
 
 @NgModule({
