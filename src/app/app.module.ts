5d91a5e3b7ec408097b95db902ebf7d58e8fc2e5
vsheva <valera.sheva@gmail.com>
2024-02-08 22:52:31 -0500

    core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from "./header/header.component";
-import {ShoppingListService} from "./shopping-list/shoppig-list.service";
 import {AppRoutingModule} from "./app-routing.module";
-import {RecipeService} from "./recipes/recipe.service";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import { HttpClientModule} from "@angular/common/http";
 import {AuthComponent} from './auth/auth.component';
-import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import {RecipesModule} from "./recipes/recipes.module";
 import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 import {SharedModule} from "./shared/shared.module";
+import {CoreModule} from "./core.module";
 
 
 @NgModule({
@@ -21,9 +19,6 @@ import {SharedModule} from "./shared/shared.module";
     HeaderComponent,
     AuthComponent,
     // DropdownDirective, //!!!no dublicate - we have it in shared.module
-    //LoadingSpinnerComponent,
-    // AlertComponent,
-    // PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -33,14 +28,9 @@ import {SharedModule} from "./shared/shared.module";
     HttpClientModule,
     RecipesModule, //!! import RecipesModule
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    CoreModule //!!
   ],
-  providers: [ShoppingListService, RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,//!!
-      useClass: AuthInterceptorService,
-      multi: true
-    }],
   bootstrap: [AppComponent],
 })
 export class AppModule {
