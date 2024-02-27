f6ed4094ceb8826c24058aba4247d27459362f7c
vsheva <valera.sheva@gmail.com>
2024-02-26 22:59:27 -0500

    shopping list reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HttpClientModule} from "@angular/common/http";
 import {SharedModule} from "./shared/shared.module";
 import {CoreModule} from "./core.module";
 import {LoggingService} from "./logging.service";
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import {LoggingService} from "./logging.service";
     AppRoutingModule,//!
     HttpClientModule,
     SharedModule,
-    CoreModule, //!
+    CoreModule,
+    StoreModule.forRoot({}, {}), //!
   ],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,22 @@
+import {Ingredient} from "../../shared/ingredient.model";
+//import {ADD_INGREDIENT} from './shopping-list.actions';
+import * as ShoppingListActions from './shopping-list.actions';
+
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ]
+};
+
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      state.ingredients.push()
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      }
+  }
+}
